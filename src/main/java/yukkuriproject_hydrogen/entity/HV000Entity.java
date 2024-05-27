3567fa122d53f298ef87748afc2599e1baa6d9f4
Osamu Miyazawa <miyazawa.o.aa@m.titech.ac.jp>
2024-05-27 19:47:54 +0900

    抽象自動車の継承先をMonsterからMobに変更。ピースフルでも召喚可能になった。

--- src/main/java/yukkuriproject_hydrogen/entity/HV000Entity.java
+++ src/main/java/yukkuriproject_hydrogen/entity/HV000Entity.java
@@ -10,12 +10,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.projectile.ThrownPotion;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
-import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
-import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
-import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
-import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.MobType;
@@ -32,28 +26,13 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.BuiltInRegistries;
 
-public class HV000Entity extends Monster {
+public class HV000Entity extends Mob {
 	public HV000Entity(EntityType<HV000Entity> type, Level world) {
 		super(type, world);
 		setMaxUpStep(1f);
 		xpReward = 0;
 		setNoAi(false);
 		setPersistenceRequired();
-	}
-
-	@Override
-	protected void registerGoals() {
-		super.registerGoals();
-		this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.2, false) {
-			@Override
-			protected boolean canPerformAttack(LivingEntity entity) {
-				return this.isTimeToAttack() && this.mob.distanceToSqr(entity) < (this.mob.getBbWidth() * this.mob.getBbWidth() + entity.getBbWidth()) && this.mob.getSensing().hasLineOfSight(entity);
-			}
-		});
-		this.goalSelector.addGoal(2, new RandomStrollGoal(this, 1));
-		this.targetSelector.addGoal(3, new HurtByTargetGoal(this));
-		this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
-		this.goalSelector.addGoal(5, new FloatGoal(this));
 	}
 
 	@Override
